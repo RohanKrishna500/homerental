db123d4d288a1a4158a34404bd8bfa39750b9f06
RohanKrishna500 <144703643+RohanKrishna500@users.noreply.github.com>
2024-11-08 22:29:57 +0530

    Update Owner.js

--- server/models/Owner.js
+++ server/models/Owner.js
@@ -1,1 +1,16 @@
+import mongoose from "mongoose";
 
+const OwnerSchema = new mongoose.Schema({
+  name: String,
+  email: {
+    type: String,
+    unique: true,
+  },
+  password: String,
+  properties: [{
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Property'
+  }]
+});
+
+export default mongoose.model("Owner", OwnerSchema);
