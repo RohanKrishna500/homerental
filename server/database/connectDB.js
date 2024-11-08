ce246ba5685bedcfebbd57f9ffbd2cb22710cb1e
RohanKrishna500 <144703643+RohanKrishna500@users.noreply.github.com>
2024-11-08 22:30:56 +0530

    Update connectDB.js

--- server/database/connectDB.js
+++ server/database/connectDB.js
@@ -1,1 +1,15 @@
+import mongoose from "mongoose";
 
+const connectDB = (url) => {
+  mongoose.set("strictQuery", true);
+  return mongoose
+    .connect(url)
+    .then(() => {
+      console.log("Database connected");
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+};
+
+export default connectDB;
