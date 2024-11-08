d222f3b2b3415da6a315a825b9733b4fdac34054
RohanKrishna500 <144703643+RohanKrishna500@users.noreply.github.com>
2024-11-08 22:30:17 +0530

    Update Property.js

--- server/models/Property.js
+++ server/models/Property.js
@@ -1,1 +1,23 @@
+import mongoose from "mongoose";
 
+const PropertySchema = new mongoose.Schema({
+  title: String,
+  address: String,
+  price: Number,
+  description: String,
+  owner: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Owner'
+  },
+  tenant: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Tenant',
+    default: null
+  },
+  isRented: {
+    type: Boolean,
+    default: false
+  }
+});
+
+export default mongoose.model("Property", PropertySchema);
